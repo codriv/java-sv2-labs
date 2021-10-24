018222e09ef72bd97594e8caa2ffb83c65b7f38b
codriv <combodriver4@yahoo.at>
2021-10-24 23:05:42 +0200

    training-solutions task: ArraysMain updated

--- training-solutions/src/main/java/arraylist/Freezer.java
+++ training-solutions/src/main/java/arraylist/Freezer.java
@@ -7,21 +7,25 @@ import java.util.List;
 public class Freezer {
     public static void main(String[] args) {
         List<String> foods = new ArrayList<>();
+        System.out.println("\nA lista:");
         String[] arrayOfFoods = {"spenót", "pörkölt", "spagetti", "tökfőzelék", "palacsinta"};
-//        System.out.println(foods.toString());
         for (String item: arrayOfFoods) {
             foods.add(item);
         }
         for (String item: foods) {
-            System.out.println(item);
+            System.out.println(" " + item);
         }
-        System.out.println(foods.size());
+        System.out.println("\nA lista mérete:");
+        System.out.println(" " + foods.size());
 
+        System.out.println("\nA listáról levettűk a pörköltet és a tökfőzeléket. A frissített lista:");
         foods.removeAll(Arrays.asList("pörkölt", "tökfőzelék"));
         for (String item: foods) {
-            System.out.println(item);
+            System.out.println(" " + item);
         }
-        System.out.println(foods.size());
+
+        System.out.println("\nA frissített lista mérete:");
+        System.out.println(" " + foods.size());
     }
 
 
