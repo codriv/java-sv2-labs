35da65730f08bb86a7ab2e360b006649a015e657
codriv <combodriver4@yahoo.at>
2021-11-02 08:08:36 +0100

    training-solutions task: enumtypes - Position

---
+++ training-solutions/src/main/java/enumtypes/firstenum/position/Position.java
@@ -0,0 +1,22 @@
+package enumtypes.firstenum.position;
+
+public enum Position {
+    PORTER(300_000, "étkezési jegy"), ANALYST(400_000, "üdülési csekk"),
+    EXECUTIVE(600_000, "telefon, laptop"), HEAD_OF_DEPARTMENT(800_000, "autó");
+
+    private final int salary;
+    private final String benefit;
+
+    Position(int salary, String benefit) {
+        this.salary = salary;
+        this.benefit = benefit;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getBenefit() {
+        return benefit;
+    }
+}
