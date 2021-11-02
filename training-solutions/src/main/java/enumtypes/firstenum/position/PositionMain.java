7f25243dad2c8971cfd4745b7713dc2bae5a0b2c
codriv <combodriver4@yahoo.at>
2021-11-02 05:43:06 +0100

    training-solutions task: enumtypes - Position

---
+++ training-solutions/src/main/java/enumtypes/firstenum/position/PositionMain.java
@@ -0,0 +1,11 @@
+package enumtypes.firstenum.position;
+
+public class PositionMain {
+    public static void main(String[] args) {
+        for (Position position: Position.values()) {
+            System.out.println(position.name() + ": " + "\n salary: " + position.getSalary() + "\n benefit: " + position.getBenefit());
+            System.out.println("\nExecutive benefit: " + Position.EXECUTIVE.getBenefit());
+            System.out.println("Index of analyst: " + Position.ANALYST.ordinal());
+        }
+    }
+}
