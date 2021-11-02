94a91d5037cb880f4a410bdaa4c87d982b685428
codriv <combodriver4@yahoo.at>
2021-11-02 08:08:57 +0100

    training-solutions task: enumtypes - SolarSystem

---
+++ training-solutions/src/main/java/enumtypes/firstenum/solarsystem/SolarSystem.java
@@ -0,0 +1,17 @@
+package enumtypes.firstenum.solarsystem;
+
+public enum SolarSystem {
+    MERCURY(0), VENUS(0), EARTH(1), MARS(2),
+    JUPITER(63), SATURN(62), URANUS(27), NEPTUNE(13),
+    PLUTO(6);
+
+    private final int numberOfSatellites;
+
+    SolarSystem(int numberOfSatellites) {
+        this.numberOfSatellites = numberOfSatellites;
+    }
+
+    public int getNumberOfSatellites() {
+        return numberOfSatellites;
+    }
+}
