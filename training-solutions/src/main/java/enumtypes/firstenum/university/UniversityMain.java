0209a44c5668776243ffc4223a80de8112be711e
codriv <combodriver4@yahoo.at>
2021-11-02 08:09:21 +0100

    training-solutions task: enumtypes - University

---
+++ training-solutions/src/main/java/enumtypes/firstenum/university/UniversityMain.java
@@ -0,0 +1,11 @@
+package enumtypes.firstenum.university;
+
+public class UniversityMain {
+    public static void main(String[] args) {
+        for (University university: University.values()) {
+            System.out.println(university.name() + ": " + university.getName());
+        }
+        System.out.println(University.SZE.getName());
+        System.out.println(University.BME.ordinal());
+    }
+}
