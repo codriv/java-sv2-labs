9fd9d27f8979c5985016831fe8144a3a6a33cbd0
codriv <combodriver4@yahoo.at>
2021-11-07 17:46:07 +0100

    training-solutions task: Junit5 test

---
+++ training-solutions/src/test/java/junit5/GentlemanTest.java
@@ -0,0 +1,15 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GentlemanTest {
+
+    @Test
+    public void testCreate(){
+        Gentleman gentleman = new Gentleman();
+        assertEquals("Hello Sándor", gentleman.sayHello("Sándor"));
+        assertEquals("Hello Anonymous", gentleman.sayHello(null));
+    }
+}
