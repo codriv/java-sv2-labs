074f4c1b1c5ee4676bcb54d5a52c060ef6e0d442
codriv <combodriver4@yahoo.at>
2021-11-28 23:45:47 +0100

    training-solutions task: algorithmsfilter - Movie

---
+++ training-solutions/src/main/java/algorithmsfilter/movie/Category.java
@@ -0,0 +1,5 @@
+package algorithmsfilter.movie;
+
+public enum Category {
+    ANIMATION, FANTASY, ACTION, THRILLER, ROMANTIC, COMEDY
+}
---
+++ training-solutions/src/main/java/algorithmsfilter/movie/VideoTheque.java
@@ -0,0 +1,25 @@
+package algorithmsfilter.movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoTheque {
+    private List<Movie> movies = new ArrayList<>();
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> getGoodMoviesWithCategoryGiven(Category category) {
+        List<Movie> goodMoviesWithCategoryGiven = new ArrayList<>();
+        for (Movie movie: movies) {
+            if (movie.getCategory() == category && movie.getRating() > 3) {
+                goodMoviesWithCategoryGiven.add(movie);
+            }
+        }
+        return goodMoviesWithCategoryGiven;
+    }
+}
