052c7d3cbd58243bfd3f745ddcb7b422f6c8b224
codriv <combodriver4@yahoo.at>
2021-12-01 13:11:18 +0100

    training-solutions task: methodchain - GrasshopperTest updated

--- training-solutions/src/test/java/methodchain/GrasshopperTest.java
+++ training-solutions/src/test/java/methodchain/GrasshopperTest.java
@@ -7,6 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class GrasshopperTest {
 
     @Test
+    void testHopOnce() {
+        Grasshopper grasshopper = new Grasshopper();
+        grasshopper.hopOnce(Direction.POSITIVE);
+        assertEquals(1, grasshopper.getPosition());
+        grasshopper.hopOnce(Direction.NEGATIVE);
+        assertEquals(0, grasshopper.getPosition());
+    }
+
+    @Test
     void testMoveFromZeroToThreeWithFiveHops() {
         Grasshopper grasshopper = new Grasshopper();
         grasshopper.moveFromZeroToThreeWithFiveHops();
