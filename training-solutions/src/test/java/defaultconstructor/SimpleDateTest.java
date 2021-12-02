e4d9ba65c70698ff6f4a04612d0c042a32ea48ae
codriv <combodriver4@yahoo.at>
2021-12-02 22:25:12 +0100

    training-solutions task: defaultconstructor - SimpleDateFormatterTest

--- training-solutions/src/test/java/defaultconstructor/SimpleDateTest.java
+++ training-solutions/src/test/java/defaultconstructor/SimpleDateTest.java
@@ -15,5 +15,13 @@ class SimpleDateTest {
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> simpleDate.setDate(1850, 5, 2));
         assertEquals("Date is not correct!", exception.getMessage());
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> simpleDate.setDate(1950, 4, 31));
+        assertEquals("Date is not correct!", exception.getMessage());
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> simpleDate.setDate(2015, 2, 29));
+        assertEquals("Date is not correct!", exception.getMessage());
+        simpleDate.setDate(2016, 2, 29);
+        assertEquals(29, simpleDate.getDay());
     }
 }
