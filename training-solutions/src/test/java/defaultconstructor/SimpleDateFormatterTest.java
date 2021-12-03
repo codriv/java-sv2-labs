cb4721ce907a6abf525a0decf4b4aa272231c55d
codriv <combodriver4@yahoo.at>
2021-12-03 20:22:27 +0100

    training-solutions task: defaultconstructor - SimpleDateFormatter updated

--- training-solutions/src/test/java/defaultconstructor/SimpleDateFormatterTest.java
+++ training-solutions/src/test/java/defaultconstructor/SimpleDateFormatterTest.java
@@ -13,46 +13,47 @@ class SimpleDateFormatterTest {
     void testFormatDateString() {
         simpleDate.setDate(1952, 3, 5);
         System.out.println(simpleDateFormatter.formatDateString(simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateString(simpleDate));
     }
 
     @Test
     void testFormatDateStringByCountryCode() {
         simpleDate.setDate(1952, 3, 5);
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
+        assertEquals("3-5-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
+        assertEquals("5-3-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
     }
 
     @Test
     void testFormatDateStringByCountryCode2() {
         simpleDate.setDate(1952, 3, 5);
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode2(CountryCode2.US, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode2(CountryCode2.HU, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode2(CountryCode2.EN, simpleDate));
+        assertEquals("3-5-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
+        assertEquals("5-3-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
 
     }
 
     @Test
     void testFormatDateStringByCountryCode3() {
         simpleDate.setDate(1952, 3, 5);
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode3(CountryCode3.US, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode3(CountryCode3.HU, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode3(CountryCode3.EN, simpleDate));
+        assertEquals("3-5-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
+        assertEquals("5-3-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
     }
 
     @Test
     void testFormatDateStringByCountryCode4() {
         simpleDate.setDate(1952, 3, 5);
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode4(CountryCode.US, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode4(CountryCode.HU, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode4(CountryCode.EN, simpleDate));
+        assertEquals("3-5-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
+        assertEquals("5-3-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
     }
 
     @Test
     void testFormatDateStringByCountryCode5() {
         simpleDate.setDate(1952, 3, 5);
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode5(CountryCode.US, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode5(CountryCode.HU, simpleDate));
-        System.out.println(simpleDateFormatter.formatDateStringByCountryCode5(CountryCode.EN, simpleDate));
+        assertEquals("3-5-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.US, simpleDate));
+        assertEquals("1952-3-5", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.HU, simpleDate));
+        assertEquals("5-3-1952", simpleDateFormatter.formatDateStringByCountryCode(CountryCode.EN, simpleDate));
     }
 }
