3e5d96e1a5d088720a9bfb13bfa7be09a0be8bca
codriv <combodriver4@yahoo.at>
2021-12-04 21:29:54 +0100

    training-solutions task: objectclass/ancestor - CommonAncestor

---
+++ training-solutions/src/main/java/objectclass/ancestor/CommonAncestor.java
@@ -0,0 +1,25 @@
+package objectclass.ancestor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonAncestor {
+
+    public static void main(String[] args) {
+        List<Object> objects = new ArrayList<>();
+        objects.add(new Fruit());
+        objects.add(new Apple());
+        objects.add(new Starking());
+        objects.add(new Vegetable());
+        objects.add("alma");
+        objects.add(12);
+        objects.add('x');
+        objects.add(LocalDate.now());
+        objects.add(new int[] {2, 5, 8, 25});
+        objects.add(Arrays.asList("string", "objektum", "ős"));
+
+        System.out.println(objects);
+    }
+}
