88770fd0e53a03c259c55609d9359f8c3a44301d
codriv <combodriver4@yahoo.at>
2022-01-01 11:44:38 +0100

    training-solutions project: enumabstract - Discount

---
+++ training-solutions/src/main/java/enumabstract/Discount.java
@@ -0,0 +1,24 @@
+package enumabstract;
+
+public enum Discount {
+
+    SPECIAL_OFFER {
+        @Override
+        public double getAmountToPay(int price, int pieces) {
+            return pieces * price * 0.8;
+        }
+    },
+
+    BLACK_FRIDAY_OFFER {
+        @Override
+        public double getAmountToPay(int price, int pieces) {
+            if (pieces <= 2) {
+                return pieces * price * 0.75;
+            } else {
+                return price * (0.5 + (pieces - 1) * 0.75);
+            }
+        }
+    };
+
+    public abstract double getAmountToPay(int price, int pieces);
+}
