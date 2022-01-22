ad2e89e879905dbd8668e84ab4896c06ecbf3e2a
codriv <combodriver4@yahoo.at>
2022-01-22 16:37:49 +0100

    training-solutions tasks: nestedclasses - NameServer

--- training-solutions/src/main/java/nestedclasses/dns/NameServer.java
+++ training-solutions/src/main/java/nestedclasses/dns/NameServer.java
@@ -1,6 +1,7 @@
 package nestedclasses.dns;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class NameServer {
@@ -20,6 +21,22 @@ public class NameServer {
             this.hostName = name;
             this.hostIp = hostIp;
         }
+
+        public String getHostName() {
+            return hostName;
+        }
+
+        public String getHostIp() {
+            return hostIp;
+        }
+    }
+
+    public String getEntryHostNameByIndex(int i) {
+        return dnsEntries.get(i).getHostName();
+    }
+
+    public String getEntryHostIpByIndex(int i) {
+        return dnsEntries.get(i).getHostIp();
     }
 
     public void addEntry(String hostName, String hostIp) {
@@ -37,5 +54,41 @@ public class NameServer {
             }
         }
         return false;
+    }
+
+    public void removeEntryByName(String hostName) {
+        Iterator<DnsEntry> iterator = dnsEntries.iterator();
+        while (iterator.hasNext()) {
+            if (hostName.equals(iterator.next().hostName)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public void removeEntryByIp(String hostIp) {
+        Iterator<DnsEntry> iterator = dnsEntries.iterator();
+        while (iterator.hasNext()) {
+            if (hostIp.equals(iterator.next().hostIp)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public String getIpByName(String hostName) {
+        for (DnsEntry dnsEntry: dnsEntries) {
+            if (hostName.equals(dnsEntry.hostName)) {
+                return dnsEntry.getHostIp();
+            }
+        }
+        throw new IllegalArgumentException("Element not found");
+    }
+
+    public String getNameByIp(String hostIp) {
+        for (DnsEntry dnsEntry: dnsEntries) {
+            if (hostIp.equals(dnsEntry.hostIp)) {
+                return dnsEntry.getHostName();
+            }
+        }
+        throw new IllegalArgumentException("Element not found");
     }
 }
--- training-solutions/src/test/java/nestedclasses/dns/NameServerTest.java
+++ training-solutions/src/test/java/nestedclasses/dns/NameServerTest.java
@@ -1,23 +1,54 @@
 package nestedclasses.dns;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameServerTest {
 
-    @Test
-    void addEntry() {
+    NameServer nameServer = new NameServer();
 
-        NameServer nameServer = new NameServer();
+    @BeforeEach
+    void init() {
         nameServer.addEntry("valami.hu", "112.152.201.136");
         nameServer.addEntry("megvalami.com", "98.112.221.36");
         nameServer.addEntry("esmegvalami.org", "2.202.21.148");
+    }
+
+    @Test
+    void addEntryTest() {
 
         assertEquals(3, nameServer.getDnsEntries().size());
 
         IllegalArgumentException iae = assertThrows(IllegalArgumentException.class,
                 ()-> nameServer.addEntry("megvalami.com", "98.112.221.36"));
         assertEquals("Already exists", iae.getMessage());
+    }
+
+    @Test
+    void removeEntryByNameTest() {
+        nameServer.removeEntryByName("megvalami.com");
+        assertEquals(2, nameServer.getDnsEntries().size());
+        assertEquals("valami.hu", nameServer.getEntryHostNameByIndex(0));
+        assertEquals("esmegvalami.org", nameServer.getEntryHostNameByIndex(1));
+    }
+
+    @Test
+    void removeEntryByIpTest() {
+        nameServer.removeEntryByIp("98.112.221.36");
+        assertEquals(2, nameServer.getDnsEntries().size());
+        assertEquals("112.152.201.136", nameServer.getEntryHostIpByIndex(0));
+        assertEquals("2.202.21.148", nameServer.getEntryHostIpByIndex(1));
+    }
+
+    @Test
+    void getNameByNameTest() {
+        assertEquals("98.112.221.36", nameServer.getIpByName("megvalami.com"));
+    }
+
+    @Test
+    void getNameByIpTest() {
+        assertEquals("megvalami.com", nameServer.getNameByIp("98.112.221.36"));
     }
 }
