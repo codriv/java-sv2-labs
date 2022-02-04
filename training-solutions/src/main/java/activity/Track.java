2250ce71eec06a69d1bb79528595738439edf241
codriv <combodriver4@yahoo.at>
2022-02-04 22:11:19 +0100

    training-solutions tasks: activity - Track

--- training-solutions/src/main/java/activity/Track.java
+++ training-solutions/src/main/java/activity/Track.java
@@ -1,9 +1,68 @@
 package activity;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Track {
 
-    private List<TrackPoint> trackPoints = new ArrayList<TrackPoint>();
+    private List<TrackPoint> trackPoints = new ArrayList<>();
+
+    public List<TrackPoint> getTrackPoints() {
+        return trackPoints;
+    }
+
+    public void addTrackPoint(TrackPoint trackPoint) {
+        trackPoints.add(trackPoint);
+    }
+
+    public Coordinate findMinimumCoordinate() {
+        double minLatitude = trackPoints.stream()
+                .min(Comparator.comparingDouble(point -> point.getCoordinate().getLatitude())).get().getCoordinate().getLatitude();
+        double minLongitude = trackPoints.stream()
+                .min(Comparator.comparingDouble(point -> point.getCoordinate().getLongitude())).get().getCoordinate().getLongitude();
+        return new Coordinate(minLatitude, minLongitude);
+    }
+
+    public Coordinate findMaximumCoordinate() {
+        double maxLatitude = trackPoints.stream()
+                .max(Comparator.comparingDouble(point -> point.getCoordinate().getLatitude())).get().getCoordinate().getLatitude();
+        double maxLongitude = trackPoints.stream()
+                .max(Comparator.comparingDouble(point -> point.getCoordinate().getLongitude())).get().getCoordinate().getLongitude();
+        return new Coordinate(maxLatitude, maxLongitude);
+    }
+
+    public double getRectangleArea() {
+        double minLat = findMinimumCoordinate().getLatitude();
+        double maxLat = findMaximumCoordinate().getLatitude();
+        double minLon = findMinimumCoordinate().getLongitude();
+        double maxLon = findMaximumCoordinate().getLongitude();
+        return (maxLat - minLat) * (maxLon - minLon);
+    }
+
+    public double getDistance() {
+        double distance = 0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            distance += trackPoints.get(i).getDistanceFrom(trackPoints.get(i - 1));
+        }
+        return distance;
+    }
+
+    public double getFullDecrease() {
+        double fullDecrease = 0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            double decrease = trackPoints.get(i).getElevation() - (trackPoints.get(i - 1).getElevation());
+            fullDecrease -= decrease < 0 ? decrease : 0;
+        }
+        return fullDecrease;
+    }
+
+    public double getFullElevation() {
+        double fullElevation = 0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            double elevation = trackPoints.get(i).getElevation() - (trackPoints.get(i - 1).getElevation());
+            fullElevation += elevation > 0 ? elevation : 0;
+        }
+        return fullElevation;
+    }
 }
--- training-solutions/src/test/java/activity/TrackTest.java
+++ training-solutions/src/test/java/activity/TrackTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TrackTest {
 
-
     public Track track = new Track();
 
     @Test
