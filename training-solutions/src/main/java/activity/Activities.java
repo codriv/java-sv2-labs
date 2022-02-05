b3befc80c23cd16b361f74fefd789a8a43e04b4e
codriv <combodriver4@yahoo.at>
2022-02-05 12:32:27 +0100

    training-solutions tasks: activity - Activities

--- training-solutions/src/main/java/activity/Activities.java
+++ training-solutions/src/main/java/activity/Activities.java
@@ -1,6 +1,8 @@
 package activity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Activities {
@@ -9,5 +11,32 @@ public class Activities {
 
     public Activities(List<Activity> activities) {
         this.activities = activities;
+    }
+
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public List<Report>distancesByTypes() {
+        List<Report> distanceByTypes = new ArrayList<>();
+        Arrays.stream(ActivityType.values()).forEach(type -> distanceByTypes.add(new Report(type, getSumOfDistances(type))));
+        return Collections.unmodifiableList(distanceByTypes);
+    }
+
+    private double getSumOfDistances(ActivityType activityType) {
+        return activities.stream().filter(act -> activityType == act.getType())
+                .mapToDouble(Activity::getDistance).sum();
+    }
+
+    public long numberOfTrackActivities() {
+        return activities.stream().filter(this::isActivityWithTrack).count();
+    }
+
+    public long numberOfWithoutTrackActivities() {
+        return activities.stream().filter(act -> !isActivityWithTrack(act)).count();
+    }
+
+    public boolean isActivityWithTrack(Activity activity) {
+        return activity.getClass() == ActivityWithTrack.class;
     }
 }
