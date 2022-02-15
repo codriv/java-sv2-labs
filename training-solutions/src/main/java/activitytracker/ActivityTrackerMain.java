5693991fb87f7162bcd462dd566e372e6a3c1bdc
codriv <combodriver4@yahoo.at>
2022-02-15 20:50:33 +0100

    training-solutions tasks: activitytracker - 4

--- training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
+++ training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -10,17 +10,17 @@ public class ActivityTrackerMain {
 
         MariaDbDataSource dataSource = new MariaDbDataSource();
 
-//        Activity activity1 = new Activity(1, LocalDateTime.of(2022, 1, 10, 10, 10), "Bicikli", ActivityType.BIKING);
-//        Activity activity2 = new Activity(2, LocalDateTime.of(2022, 1, 20, 20, 20), "Fut", ActivityType.RUNNING);
-//        Activity activity3 = new Activity(3, LocalDateTime.of(2022, 2, 10, 5, 5), "Tura", ActivityType.HIKING);
+        Activity activity1 = new Activity(1, LocalDateTime.of(2022, 1, 10, 10, 10), "Bicikli", ActivityType.BIKING);
+        Activity activity2 = new Activity(2, LocalDateTime.of(2022, 1, 20, 20, 20), "Fut", ActivityType.RUNNING);
+        Activity activity3 = new Activity(3, LocalDateTime.of(2022, 2, 10, 5, 5), "Tura", ActivityType.HIKING);
 
         ActivityRepository ar = new ActivityRepository(dataSource);
 
         ar.connect("localhost", 3306, "activitytracker", "root", "64o1qNpG8m");
-//        ar.creteTable();
-//        ar.insert(activity1);
-//        ar.insert(activity2);
-//        ar.insert(activity3);
+        ar.creteTable();
+        ar.insert(activity1);
+        ar.insert(activity2);
+        ar.insert(activity3);
         System.out.println(ar.getActivityById(2));
         System.out.println(ar.getActivies());
     }
