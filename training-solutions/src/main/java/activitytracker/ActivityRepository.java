0628082cda93f88286f4e02d634ab192addc57a2
codriv <combodriver4@yahoo.at>
2022-02-15 19:46:04 +0100

    training-solutions tasks: activitytracker - 2

--- training-solutions/src/main/java/activitytracker/ActivityRepository.java
+++ training-solutions/src/main/java/activitytracker/ActivityRepository.java
@@ -2,33 +2,22 @@ package activitytracker;
 
 import org.mariadb.jdbc.Connection;
 import org.mariadb.jdbc.MariaDbDataSource;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class ActivityRepository {
 
     private MariaDbDataSource dataSource;
 
-    public ActivityRepository() {
-        dataSource = new MariaDbDataSource();
+    public ActivityRepository(MariaDbDataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public void connect(String host, int port, String database) {
+    public void connect(String host, int port, String database, String user, String password) {
         try {
             dataSource.setUrl(String.format("jdbc:mariadb://%s:%d/%s?useUnicode=true", host, port, database));
-            dataSource.setUser("root");
-            dataSource.setPassword("64o1qNpG8m");
-        } catch (SQLException sqle) {
-            throw new IllegalStateException("Cannot reach database!", sqle);
-        }
-    }
-
-    public void sqlQuery() {
-        try (Connection connection = (Connection) dataSource.getConnection();
-             Statement stmt = connection.createStatement()) {
-            stmt.execute("create table `activities` (`id` int auto_increment, `startTime` datetime," +
-                    "`activity_desc` varchar(255), `activity_type` varchar(20), constraint id_auto primary key (id))");
+            dataSource.setUser(user);
+            dataSource.setPassword(password);
         } catch (SQLException sqle) {
             throw new IllegalStateException("Cannot reach database!", sqle);
         }
@@ -36,9 +25,9 @@ public class ActivityRepository {
 
     public void creteTable() {
         try (Connection connection = (Connection) dataSource.getConnection();
-             PreparedStatement stmt = connection.prepareStatement(
-                     "create table activities (`id` int auto_increment, `startTime` datetime, `activity_desc` varchar(255), `activity_type` varchar(20), constraint id_auto primary key (id))")) {
-            stmt.execute();
+             Statement stmt = connection.createStatement()) {
+            stmt.execute("create table activities (`id` int auto_increment, `startTime` datetime," +
+                    "`activity_desc` varchar(255),`activity_type` varchar(20), constraint id_auto primary key (id))");
         } catch (SQLException sqle) {
             throw new IllegalStateException("Cannot reach database!", sqle);
         }
@@ -47,7 +36,7 @@ public class ActivityRepository {
     public void insert(Activity activity) {
         try (Connection connection = (Connection) dataSource.getConnection();
              PreparedStatement stmt = connection.prepareStatement("insert into activities (startTime, activity_desc, activity_type) values (?, ?, ?)")) {
-            stmt.setString(1, activity.getStartTime().toString());
+            stmt.setTimestamp(1, Timestamp.valueOf(activity.getStartTime()));
             stmt.setString(2, activity.getDesc());
             stmt.setString(3, activity.getType().toString());
             stmt.execute();
--- training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
+++ training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -1,18 +1,22 @@
 package activitytracker;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+
 import java.time.LocalDateTime;
 
 public class ActivityTrackerMain {
 
     public static void main(String[] args) {
+
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+
         Activity activity1 = new Activity(1, LocalDateTime.of(2022, 1, 10, 10, 10), "Bicikli", ActivityType.BIKING);
         Activity activity2 = new Activity(2, LocalDateTime.of(2022, 1, 20, 20, 20), "Fut", ActivityType.RUNNING);
         Activity activity3 = new Activity(3, LocalDateTime.of(2022, 2, 10, 5, 5), "Tura", ActivityType.HIKING);
 
-        ActivityRepository ar = new ActivityRepository();
+        ActivityRepository ar = new ActivityRepository(dataSource);
 
-        ar.connect("localhost", 3306, "activitytracker");
-//        ar.sqlQuery();
+        ar.connect("localhost", 3306, "activitytracker", "root", "64o1qNpG8m");
         ar.creteTable();
         ar.insert(activity1);
         ar.insert(activity2);
